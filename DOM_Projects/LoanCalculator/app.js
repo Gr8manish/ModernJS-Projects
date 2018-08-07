4ee17386f7e85e1c795eb78a6135dac9afced16b
Manish Menaria <the.manish.menaria@gmail.com>
2018-08-07 12:49:57 +0530

    DOM Projects: Loan Calculator

---
+++ DOM_Projects/LoanCalculator/app.js
@@ -0,0 +1,67 @@
+// Listen for submit
+document.getElementById('loan-form').addEventListener('submit', function(e){
+    document.getElementById('loading').style.display = 'block';
+    document.getElementById('results').style.display = 'none';
+
+    setTimeout(calculateResults, 2000);
+    e.preventDefault();
+});
+
+// Calculate Results
+function calculateResults(){
+    console.log('Calculating...');
+
+    // UI Vars
+    const amount = document.getElementById('amount');
+    const interest = document.getElementById('interest');
+    const years = document.getElementById('years');
+    const monthlyPayment = document.getElementById('monthly-payment');
+    const totalPayment = document.getElementById('total-payment');
+    const totalInterest = document.getElementById('total-interest');
+  
+    const principal = parseFloat(amount.value);
+    const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+    const calculatedPayments = parseFloat(years.value) * 12;
+  
+    // Compute monthly payment
+    const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+    const monthly = (principal*x*calculatedInterest)/(x-1);
+  
+    if(isFinite(monthly)) {
+      monthlyPayment.value = monthly.toFixed(2);
+      totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+      totalInterest.value = ((monthly * calculatedPayments)-principal).toFixed(2);
+      document.getElementById('results').style.display = 'block';
+    } else {
+      showError('Please check your numbers');
+    }
+
+    document.getElementById('loading').style.display = 'none';
+}
+
+
+// Show error
+function showError(error){
+    const errorDIv = document.createElement('div');
+
+    const card = document.querySelector('.card');
+    const heading = document.querySelector('.heading');
+
+    errorDIv.className = 'alert alert-danger';
+
+    errorDIv.appendChild(document.createTextNode(error));
+    card.insertBefore(errorDIv, heading);
+
+    setTimeout(clearError, 3000);
+}
+
+
+// Clear Error
+function clearError(){
+    document.querySelector('.alert').remove();
+}
+
+function sleep(seconds){
+    var waitUntil = new Date().getTime() + seconds*1000;
+    while(new Date().getTime() < waitUntil) true;
+}
